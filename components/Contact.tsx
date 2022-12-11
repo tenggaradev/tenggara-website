77b6bdb9bed368f7d00c397147faa6e7ed590bb7
tribagus <tri.bagus21@gmail.com>
2022-12-11 12:55:17 +0700

    add emailjs and send email function

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,7 +1,37 @@
+import React, { useRef } from "react"
+import emailjs from "@emailjs/browser"
+import { ToastContainer, toast } from "react-toastify"
 // style
 import styles from "../styles/Contact.module.scss"
+import "react-toastify/dist/ReactToastify.css"
 
 const Contact = () => {
+  const notify = () =>
+    toast.info("Message Sent! \r\n we will reach you back shortly")
+  const form: any = useRef()
+
+  const sendEmail = (e: any) => {
+    e.preventDefault()
+
+    emailjs
+      .sendForm(
+        "service_rrwnblf",
+        "template_uonm3vb",
+        form.current,
+        "9aYefTHRkGOeaGETQ"
+      )
+      .then(
+        (result) => {
+          console.log(result.text)
+          e.target.reset()
+          notify()
+        },
+        (error) => {
+          console.log(error.text)
+        }
+      )
+  }
+
   return (
     <section className={styles.contact} id="contact">
       <div className={styles.container}>
@@ -13,17 +43,20 @@ const Contact = () => {
             excepturi similique soluta.
           </p>
         </div>
-        <form className={styles.message}>
-          <input type="text" placeholder="Name" />
-          <input type="text" placeholder="Phone" />
-          <textarea
-            name="message"
-            id="message"
-            placeholder="Message"
-          ></textarea>
-          <button>Send</button>
+        <form className={styles.message} ref={form} onSubmit={sendEmail}>
+          <input type="text" name="user_name" placeholder="Name" required />
+          <input type="email" name="user_email" placeholder="Email" required />
+          <textarea name="message" placeholder="Message" required></textarea>
+          <button type="submit">Send</button>
         </form>
       </div>
+      <ToastContainer
+        position="top-right"
+        autoClose={6000}
+        closeOnClick={false}
+        hideProgressBar={true}
+        theme="colored"
+      />
     </section>
   )
 }
